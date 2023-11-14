0fcf889d4723798581a5458a2aff9373a4e8e2bd
Vic Samoylik <v.samojlik@gmail.com>
2023-11-14 14:33:24 +0300

    feat: add basic layout to misc section

--- webpack.config.js
+++ webpack.config.js
@@ -2,8 +2,6 @@ const config = {
 	mode: 'production',
 	entry: {
 		index: './src/js/index.js'
-		// contacts: './src/js/contacts.js',
-		// about: './src/js/about.js',
 	},
 	output: {
 		filename: '[name].bundle.js',
